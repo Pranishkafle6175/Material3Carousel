3760503c59778e73787a57041ba5d2f84f5a153e
Pranish <pranishkafle1@gmail.com>
2023-10-02 22:36:06 +0545

    Carousel Made

--- app/src/main/java/com/example/carousel/MainActivity.kt
+++ app/src/main/java/com/example/carousel/MainActivity.kt
@@ -2,9 +2,16 @@ package com.example.carousel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.carousel.databinding.ActivityMainBinding
+import com.google.android.material.carousel.CarouselLayoutManager
 
 class MainActivity : AppCompatActivity() {
+
+    private var list = ArrayList<Model>()
+    private val context=this
+
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +19,20 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        list.add(Model(R.drawable.nature,"Foreground"))
+        list.add(Model(R.drawable.nature2,"Foreground"))
+        list.add(Model(R.drawable.nature3,"Foreground"))
+        list.add(Model(R.drawable.nature2,"Foreground"))
+        list.add(Model(R.drawable.nature,"Foreground"))
+        list.add(Model(R.drawable.nature3,"Foreground"))
+        list.add(Model(R.drawable.nature2,"Foreground"))
+        list.add(Model(R.drawable.nature,"Foreground"))
+
+        val adapter= CarouselAdapter(list, context)
+
+        binding.carouselRecylerView.setLayoutManager(CarouselLayoutManager())
+        binding.carouselRecylerView.adapter=adapter
 
 
     }
